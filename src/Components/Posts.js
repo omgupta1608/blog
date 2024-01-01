e704c758f239e656d8ca5b87c2c269e2060d24fa
Om Gupta <omgupta1608@gmail.com>
2024-01-01 23:19:56 +0530

    added filtering logic

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import Post from './Post';
+import Post from "./Post";
 
-export default ({posts}) => {
-  const showPost = posts.map(post => <Post key={post.id} post={post} />);
+const items = [
+  { id: 1, name: "Tech/CS", category: "Tech/CS" },
+  { id: 2, name: "Travel", category: "Travel" },
+];
 
-  return <div>{showPost}</div>
-}
+export default ({ posts }) => {
+  const [tagFilter, setTagFilter] = React.useState("All");
+  const [showPost, setShowPost] = React.useState(
+    posts.map((post) => {
+      if (tagFilter == "All") {
+        return <Post key={post.id} post={post} />;
+      } else if (post["topic"] == tagFilter)
+        return <Post key={post.id} post={post} />;
+    })
+  );
+
+  React.useEffect(() => {
+    console.log("inside use effect: ", tagFilter)
+    const newShowPost = posts.map((post) => {
+      if (tagFilter == "All") {
+        return <Post key={post.id} post={post} />;
+      } else if (post["topic"] == tagFilter)
+        return <Post key={post.id} post={post} />;
+    });
+    setShowPost(newShowPost);
+  }, [tagFilter]);
+
+  return (
+    <div>
+      <div id="filter-bar">
+      <label>
+        <input
+          type="radio"
+          name="filter"
+          value="all"
+          onChange={() => setTagFilter('All')}
+          defaultChecked
+        /> All
+      </label>
+        {items.map((category) => (
+          <label key={category.id}>
+            <input
+              type="radio"
+              name="filter"
+              value={category.category}
+              onChange={() => setTagFilter(category.category)}
+            />{" "}
+            {category.name}
+          </label>
+        ))}
+      </div>
+      <div>{showPost}</div>
+    </div>
+  );
+};
